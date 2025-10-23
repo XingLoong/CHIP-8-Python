56160212abd3aea7b8f654078af51f20a8f02504
XingLoong <xing.loong@gmail.com>
2025-10-23 21:04:03 +1100

    Revert "skeleton up"
    This reverts commit 73ae6622352156a65be630772a4c6e03114373f8.

--- main.py
+++
@@ -1,6 +0,0 @@
-from cpu import CPU
-
-chip8 = CPU()
-print("Memory size:", len(chip8.memory))
-print("Display size:", len(chip8.display_buffer), "rows x", len(chip8.display_buffer[0]), "cols")
-print("Program Counter:", hex(chip8.PC))
